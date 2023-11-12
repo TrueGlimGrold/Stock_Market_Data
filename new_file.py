0e9110df41771cfafb3a773cf787e820dbca991c
LukeBenjaminBullock <bul22006@byui.edu>
2023-11-12 11:21:56 -0700

    added a type of data searching

--- new_file.py
+++ new_file.py
@@ -58,42 +58,74 @@ titled_columns = {"filename": filename_list,
 data = pd.DataFrame(titled_columns)
 
 # Function to analyze patterns
+# Function to analyze patterns
 def analyze_patterns(df):
-    patterns = {'increase': 0, 'decrease': 0, 'increase_and_lead_to_higher': 0, 'decrease_and_lead_to_higher': 0}
+    patterns = {'increase_streak': 0, 'decrease_streak': 0, 'lead_to_higher': True}
+
+    # Variables to track streaks
+    increase_streak = 0
+    decrease_streak = 0
 
     for i in range(1, len(df)):
         if df['open'][i] > df['open'][i - 1]:
-            patterns['increase'] += 1
+            increase_streak += 1
+            decrease_streak = 0
         elif df['open'][i] < df['open'][i - 1]:
-            patterns['decrease'] += 1
+            decrease_streak += 1
+            increase_streak = 0
+        else:
+            increase_streak = 0
+            decrease_streak = 0
 
-    for i in range(len(df) - 252):  # Check one year later
-        if df['open'][i + 252] > df['open'][i]:
-            patterns['increase_and_lead_to_higher'] += 1
-        elif df['open'][i + 252] < df['open'][i]:
-            patterns['decrease_and_lead_to_higher'] += 1
+    # Check one year later
+    try:
+        if df['open'][len(df) - 1] > df['open'][len(df) - 252]:
+            patterns['lead_to_higher'] = False
+    except: print(" --- No data within a year ---")
+
+    # Update patterns dictionary
+    patterns['increase_streak'] = increase_streak
+    patterns['decrease_streak'] = decrease_streak
 
     print(f"Analyzed {df['filename'].iloc[0]}")
     return patterns
 
-# Analyze patterns for each file
-result_list = []
 unique_filenames = data['filename'].unique()
+
+# Initialize result arrays
+full_size_array = {}
+result_array = {}
 
 for filename in unique_filenames:
     df = data[data['filename'] == filename].reset_index(drop=True)
     patterns = analyze_patterns(df)
-    result_list.append({'filename': filename, **patterns})
 
-# Convert the result list to a DataFrame
-result_df = pd.DataFrame(result_list)
+    # Initialize result arrays for the streak length if not already done
+    if patterns['increase_streak'] not in result_array:
+        result_array[patterns['increase_streak']] = 0
+        full_size_array[patterns['increase_streak']] = 0
 
-# Calculate percentages
-result_df['increase_percentage'] = result_df['increase_and_lead_to_higher'] / result_df['increase'] * 100
-result_df['decrease_percentage'] = result_df['decrease_and_lead_to_higher'] / result_df['decrease'] * 100
+    # Update result arrays
+    if patterns['increase_streak'] > 0 and patterns['lead_to_higher']:
+        result_array[patterns['increase_streak']] += 1
+        full_size_array[patterns['increase_streak']] += 1
+    else:
+        full_size_array[patterns['increase_streak']] += 1
 
-# Sort by decrease_percentage in descending order
-result_df = result_df.sort_values(by='decrease_percentage', ascending=False)
+    print(patterns)
 
-# Print the result DataFrame
-print(result_df)
+# Analyze results
+for streak_length in full_size_array:
+    if result_array[streak_length] > 0:
+        success_rate = result_array[streak_length] / full_size_array[streak_length]
+        print(f"Increase Streak: {streak_length}, Success Rate: {success_rate * 100:.2F}%")
+    else:
+        print(f"Increase Streak: {streak_length}, No successful outcomes")
+        
+        
+for streak_length in full_size_array:
+    if result_array[streak_length] < 0:
+        success_rate = result_array[streak_length] / full_size_array[streak_length]
+        print(f"Decrease Streak: {streak_length}, Success Rate: {success_rate * 100:.2F}%")
+    else:
+        print(f"Decrease Streak: {streak_length}, No successful outcomes")
