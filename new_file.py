4644a829a4ffa64769fb1553110e81ad30eb1b78
LukeBenjaminBullock <bul22006@byui.edu>
2023-11-12 16:14:16 -0700

    trying to add a progress bar

--- new_file.py
+++ new_file.py
@@ -2,6 +2,8 @@ import pandas as pd
 import os
 import csv
 from datetime import datetime, timedelta
+import sys
+import progressbar 
 
 os.system('cls')
 
@@ -57,8 +59,11 @@ titled_columns = {"filename": filename_list,
 # Start using the data
 data = pd.DataFrame(titled_columns)
 
-# Function to analyze patterns
-# Function to analyze patterns
+# Function to clear the last line in the console
+def clear_last_line():
+    sys.stdout.write('\033[F')  # Move the cursor up one line
+    sys.stdout.write('\033[K')  # Clear the line
+
 def analyze_patterns(df):
     patterns = {'increase_streak': 0, 'decrease_streak': 0, 'lead_to_higher': True}
 
@@ -79,7 +84,7 @@ def analyze_patterns(df):
 
     # Check one year later
     try:
-        if df['open'][len(df) - 1] > df['open'][len(df) - 252]:
+        if df['open'][252] > df['open'][0]:
             patterns['lead_to_higher'] = False
     except: print(" --- No data within a year ---")
 
@@ -87,45 +92,74 @@ def analyze_patterns(df):
     patterns['increase_streak'] = increase_streak
     patterns['decrease_streak'] = decrease_streak
 
-    print(f"Analyzed {df['filename'].iloc[0]}")
+    # print(f"Analyzed {df['filename'].iloc[0]}")
     return patterns
 
 unique_filenames = data['filename'].unique()
 
 # Initialize result arrays
-full_size_array = {}
-result_array = {}
+increase_result_array = {}
+increase_full_size_array = {}
+decrease_result_array = {}
+decrease_full_size_array = {}
 
-for filename in unique_filenames:
+# Create a progress bar for file processing
+widgets_files = [progressbar.Percentage(), ' ', progressbar.Counter(), ' ',
+                    progressbar.Bar(), ' ', progressbar.Timer(), ' ']
+progress_bar_files = progressbar.ProgressBar(widgets=widgets_files, maxval=len(unique_filenames)).start()
+
+# Initialize result arrays
+increase_result_array = {}
+increase_full_size_array = {}
+decrease_result_array = {}
+decrease_full_size_array = {}
+
+for idx, filename in enumerate(unique_filenames):
     df = data[data['filename'] == filename].reset_index(drop=True)
     patterns = analyze_patterns(df)
 
     # Initialize result arrays for the streak length if not already done
-    if patterns['increase_streak'] not in result_array:
-        result_array[patterns['increase_streak']] = 0
-        full_size_array[patterns['increase_streak']] = 0
+    if patterns['increase_streak'] not in increase_result_array:
+        increase_result_array[patterns['increase_streak']] = 0
+        increase_full_size_array[patterns['increase_streak']] = 0
 
-    # Update result arrays
+    if patterns['decrease_streak'] not in decrease_result_array:
+        decrease_result_array[patterns['decrease_streak']] = 0
+        decrease_full_size_array[patterns['decrease_streak']] = 0
+
+    # Update result arrays for increase streaks
     if patterns['increase_streak'] > 0 and patterns['lead_to_higher']:
-        result_array[patterns['increase_streak']] += 1
-        full_size_array[patterns['increase_streak']] += 1
+        increase_result_array[patterns['increase_streak']] += 1
+        increase_full_size_array[patterns['increase_streak']] += 1
     else:
-        full_size_array[patterns['increase_streak']] += 1
+        increase_full_size_array[patterns['increase_streak']] += 1
 
-    print(patterns)
+    # Update result arrays for decrease streaks
+    if patterns['decrease_streak'] > 0 and not patterns['lead_to_higher']:
+        decrease_result_array[patterns['decrease_streak']] += 1
+        decrease_full_size_array[patterns['decrease_streak']] += 1
+    else:
+        decrease_full_size_array[patterns['decrease_streak']] += 1
 
-# Analyze results
-for streak_length in full_size_array:
-    if result_array[streak_length] > 0:
-        success_rate = result_array[streak_length] / full_size_array[streak_length]
+    # Clear the last line before updating progress bar for file processing
+    clear_last_line()
+    progress_bar_files.update(idx)
+
+# Finish the progress bar for file processing
+progress_bar_files.finish()
+
+# Analyze results for increase streaks
+for streak_length in increase_full_size_array:
+    if increase_result_array[streak_length] > 0:
+        success_rate = increase_result_array[streak_length] / increase_full_size_array[streak_length]
         print(f"Increase Streak: {streak_length}, Success Rate: {success_rate * 100:.2F}%")
     else:
         print(f"Increase Streak: {streak_length}, No successful outcomes")
-        
-        
-for streak_length in full_size_array:
-    if result_array[streak_length] < 0:
-        success_rate = result_array[streak_length] / full_size_array[streak_length]
+
+# Analyze results for decrease streaks
+for streak_length in decrease_full_size_array:
+    if decrease_result_array[streak_length] > 0:
+        success_rate = decrease_result_array[streak_length] / decrease_full_size_array[streak_length]
         print(f"Decrease Streak: {streak_length}, Success Rate: {success_rate * 100:.2F}%")
     else:
         print(f"Decrease Streak: {streak_length}, No successful outcomes")
